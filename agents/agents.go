c1d5cc09bfc06a6b646bfb131ca33f0eb1a205ec
Adam Szmajdziński <a.szmajdzinski@gmail.com>
2025-03-13 17:19:44 +0100

    Move ExpertsTeam to a separate file

--- agents/agents.go
+++ agents/agents.go
@@ -2,8 +2,6 @@ package agents
 
 import (
 	"context"
-	"fmt"
-	"sync"
 
 	"example.com/web-app-creator/llm"
 )
@@ -26,53 +24,4 @@ func (a Agent) Chat(ctx context.Context, msg string) (string, error) {
 	// TODO: add logger
 
 	return ans.Content, nil
-}
-
-type ExpertsTeam struct {
-	Experts []Agent
-}
-
-type ExpertAnswer struct {
-	Answer string
-	Error  error
-}
-
-func (et *ExpertsTeam) Ask(ctx context.Context, msg string) []ExpertAnswer {
-	type result struct {
-		index  int
-		answer string
-		error  error
-	}
-
-	ch := make(chan result, len(et.Experts))
-	var wg sync.WaitGroup
-
-	for i, a := range et.Experts {
-		wg.Add(1)
-
-		go func(index int, agent Agent) {
-			defer wg.Done()
-			ans, err := agent.Chat(ctx, msg)
-			if err != nil {
-				ch <- result{index: index, error: fmt.Errorf("cannot get response from agent %s: %w", agent.Name, err)}
-				return
-			}
-
-			ch <- result{index: index, answer: ans}
-		}(i, a)
-	}
-
-	wg.Wait()
-	close(ch)
-
-	answers := make([]ExpertAnswer, len(et.Experts))
-
-	for res := range ch {
-		answers[res.index] = ExpertAnswer{
-			Answer: res.answer,
-			Error:  res.error,
-		}
-	}
-
-	return answers
 }
